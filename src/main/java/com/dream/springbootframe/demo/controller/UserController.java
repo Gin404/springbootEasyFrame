777fe7375eb413aa295b6d7278f686e565d92ca3
liweiyi <liweiyi@guolele.com>
2018-04-04 14:23:47 +0800

    添加缓存

--- src/main/java/com/dream/springbootframe/demo/controller/UserController.java
+++ src/main/java/com/dream/springbootframe/demo/controller/UserController.java
@@ -1,18 +1,18 @@
 package com.dream.springbootframe.demo.controller;
 
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.dream.springbootframe.demo.entity.DemoUser;
+import com.dream.springbootframe.demo.service.DemoUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.dream.springbootframe.demo.entity.DemoUser;
-import com.dream.springbootframe.demo.service.DemoUserService;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 
@@ -39,6 +39,8 @@ public class UserController {
 		model.addAttribute("user", user);
 		return "addpage";
 	}
+	//清除缓存
+	@CacheEvict(value = "userlist", allEntries = true, beforeInvocation = true)
 	@RequestMapping("/add")
 	public String addUser(DemoUser user) {
 		demoUserService.addUser(user);
@@ -48,6 +50,8 @@ public class UserController {
 	/**
 	 * 删
 	 */
+	//清除缓存
+	@CacheEvict(value = "userlist", allEntries = true, beforeInvocation = true)
 	@RequestMapping("/delete")
 	public String delete(String id) {
 		demoUserService.deleteUser(id);
@@ -57,6 +61,8 @@ public class UserController {
 	/**
 	 * 改
 	 */
+	//清除缓存
+	@CacheEvict(value = "userlist", allEntries = true, beforeInvocation = true)
 	@RequestMapping("/update")
 	public String updateUser(DemoUser user) {
 		demoUserService.updateUser(user);
@@ -70,20 +76,12 @@ public class UserController {
 	public String listUser() {
 		return "listpage";
 	}
+	//开启缓存
+	@Cacheable(value = "userlist")
 	@RequestMapping("/list")
 	@ResponseBody
 	public List<Map<String, Object>> selectAllUser() {
 		return demoUserService.selectAllUser();
 	}
 
-	/**
-	 * 测试富文本
-	 */
-	@RequestMapping("/test")
-	public String test() {
-		return "text";
-	}
-	
-	
-	
 }
