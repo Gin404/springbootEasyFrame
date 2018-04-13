a6d4f5c6adf030eb1089c780c196592273f21e46
dream-broken <31562882+dream-broken@users.noreply.github.com>
2018-04-13 18:47:49 +0800

    Create DemoUser.java

---
+++ src/main/java/com/dream/springbootframe/demo/zsgzdemo/entity/DemoUser.java
@@ -0,0 +1,18 @@
+package com.dream.springbootframe.demo.zsgzdemo.entity;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author Dream
+ * 2018年1月31日
+ *
+ *
+ */
+@Data
+public class DemoUser {
+
+	private String id;
+	private String name;
+	
+}
