5886a6d8f45f828e992791823b1ea61c556fd66a
liweiyi <liweiyi@guolele.com>
2018-05-25 18:01:04 +0800

    JMS activeMQ & RabbitMQ(AMQP)

--- src/main/java/com/dream/springbootframe/App.java
+++ src/main/java/com/dream/springbootframe/App.java
@@ -1,6 +1,5 @@
 package com.dream.springbootframe;
 
-import com.dream.springbootframe.config.netty.NettyService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,7 +19,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 //开启jpa
 @EnableJpaRepositories("com.dream.springbootframe.jpa")
 public class App implements CommandLineRunner {
-	
+
+    //JMS activeMQ
+    /*@Autowired
+    private JmsTemplate jmsTemplate;*/
+
+    //RabbitMQ(AMQP)
+    /*@Autowired
+    private RabbitTemplate rabbitTemplate;*/
+
 	public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
@@ -28,6 +35,12 @@ public class App implements CommandLineRunner {
     //实现CommandLineRunner 重写run方法 这里放了netty的启动
     @Override
     public void run(String... args) throws Exception {
-        new NettyService();
+        //new NettyService();
+
+        //JMS activeMQ 发送消息 (目的地, 消息)
+        //jmsTemplate.send("sendMsg", new Msg());
+
+        //RabbitMQ(AMQP) 发送消息 (目的地, 消息)
+        //rabbitTemplate.convertAndSend("sendMsg", "消息");
     }
 }
